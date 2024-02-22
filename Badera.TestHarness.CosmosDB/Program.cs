35e7b6e135aea3632bc1086b9badf82ef28126f1
Andy Badera <andybadera@microsoft.com>
2024-02-22 13:18:13 -0600

    Add project files.

---
+++ Badera.TestHarness.CosmosDB/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos;
+
+CosmosClient cosmosClient = new CosmosClient("AccountEndpoint={YOUR_ENDPOINT};AccountKey={YOUR_KEY}");
+
+try
+{
+    cosmosClient.CreateDatabaseIfNotExistsAsync("{EXPECTED_DB_NAME}").Wait();
+}
+catch (CosmosException cex)
+{
+
+}
+catch (AggregateException aex)
+{
+
+}
+catch (Exception ex)
+{
+
+}
